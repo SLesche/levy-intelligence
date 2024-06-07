8fb2350dadaeba72bd2375bf252def350bdd4a4b
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2024-06-07 02:22:27 +0200

    save per subject

---
+++ investigating_results.R
@@ -0,0 +1,3 @@
+library(dplyr)
+
+model_results <- read.csv("data/levy_data/posner/levy_mat_parameter.csv")
--- reading_data.R
+++ reading_data.R
@@ -20,6 +20,23 @@ few_trials <- posner_ni %>%
 posner_ni <- posner_ni %>% 
   filter(Subject != few_trials)
 
+# Need to save per participant with sub, cond, response, rt,
+levy_data <- posner_ni %>% 
+  select(
+    subject = Subject,
+    condition = condNEW,
+    resp = Accuracy,
+    rt = RTms
+  )
+
+filepath <- "./data/levy_data/posner/posner_ni_data_subject"
+
+levy_data %>%
+  group_by(subject) %>%
+  tidyr::nest() %>%
+  mutate(filename = paste0(filepath, subject, ".csv")) %>%
+  purrr::pwalk(~write.csv(..2, file = ..3, row.names = FALSE))
+
 true_rt = posner_ni$RT
 
 hist(true_rt, breaks = 100, xlim = c(0, 5))
